ad8931e53cf74af034ecbb98a2d0c91cd0843ce7
Philipp Ilievski <philipp.ilievski@gmail.com>
2022-01-13 08:38:31 +0100

    Added loginpage for waiter + UI

---
+++ JustASecond/JustASecond.Web/Data/ModelViews/OrderView.cs
@@ -0,0 +1,12 @@
+﻿using JustASecond.Web.Data.Models;
+
+namespace JustASecond.Web.Data.ModelViews
+{
+    public class OrderView
+    {
+        public int Id { get; set; }
+        public CustomerView Customer { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public OrderPosition OrderPosition { get; set; }
+    }
+}
--- JustASecond/JustASecond.Web/Data/Models/Order.cs
+++ JustASecond/JustASecond.Web/Data/Models/Order.cs
@@ -3,6 +3,8 @@
     public class Order
     {
         public string? Id { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime? CreatedDate { get; set; }
         public virtual ICollection<OrderPosition>? OrderPositions { get; set; }
     }
 }
