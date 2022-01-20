5a24b74c0383f197db4e24c6eceabe4a1c02b8e1
Fabio Drobic <fabio@drobic.com>
2022-01-20 15:54:39 +0100

    Removed Name-Props from Order

--- JustASecond.DAL/Data/Models/Order.cs
+++ JustASecond.DAL/Data/Models/Order.cs
@@ -3,9 +3,6 @@
     public class Order
     {
         public int? Id { get; set; }
-        public string? Firstname { get; set; }
-        public string? Lastname { get; set; }
-
         public DateTime? CreatedDate { get; set; }
         public virtual ICollection<OrderPosition>? OrderPositions { get; set; }
     }
--- JustASecond.DAL/Migrations/20220120145401_Removed Name-Props from Order.Designer.cs
+++ JustASecond.DAL/Migrations/20220120145401_Removed Name-Props from Order.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using JustASecond.DAL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JustASecond.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220120145401_Removed Name-Props from Order")]
+    partial class RemovedNamePropsfromOrder
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
